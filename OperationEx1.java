840bc61187e9fbeeb0cf6b561b0c7e10e3362812
yangseopjeong <72899485+yangseopjeong@users.noreply.github.com>
2021-02-05 12:45:45 +0900

    Update OperationEx1.java

--- OperationEx1.java
+++ OperationEx1.java
@@ -13,12 +13,18 @@ public class OperationEx1 {
 		int apple_one=apple/child;
 		int apple_remain = apple%child;
 		System.out.println("한사람당 배급된 사과수:"+apple_one+"\n남은 사과수:"+ apple_remain);
+		// 증감 연산자
 		int num =1;
 		System.out.println(num++);
 		System.out.println(++num);
+		//부정연산자
+		boolean tf=true;
+		tf = !tf;
+		System.out.println(tf);
 	}
 
 }
+
 
 
 // 결과 콘솔창
@@ -27,5 +33,6 @@ public class OperationEx1 {
 // 남은 사과수:11
 //1
 //3
+//false
 
 
