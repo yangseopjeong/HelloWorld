d74fa2bfcdc472185057cf9128f9d57271c4748d
yangseopjeong <72899485+yangseopjeong@users.noreply.github.com>
2021-02-05 12:08:00 +0900

    Update OperationEx1.java

--- OperationEx1.java
+++ OperationEx1.java
@@ -13,13 +13,19 @@ public class OperationEx1 {
 		int apple_one=apple/child;
 		int apple_remain = apple%child;
 		System.out.println("한사람당 배급된 사과수:"+apple_one+"\n남은 사과수:"+ apple_remain);
+		int num =1;
+		System.out.println(num++);
+		System.out.println(++num);
 	}
 
 }
+
 
 // 결과 콘솔창
 // 결과값:9
 // 한사람당 배급된 사과수:6
 // 남은 사과수:11
+//1
+//3
 
 
