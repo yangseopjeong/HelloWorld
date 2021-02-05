d8f8c69c40a35535d363a4d456b52748e0d833e6
yangseopjeong <72899485+yangseopjeong@users.noreply.github.com>
2021-02-05 10:57:36 +0900

    Update javaex1.java

--- javaex1.java
+++ javaex1.java
@@ -11,7 +11,7 @@ public class javaex1 {
 		int int_num2 =1200;
 		byte byte_num= (byte)int_num2;
 		System.out.println(byte_num);
-		//바이트를 잘라서 1바이트 잘라서 저장하기 때문에 값이 다르게 바뀔수도 있다
+		//형의 크기만큼 바이트를 잘라서 저장하기 때문에 값이 다르게 바뀔수도 있다
 		// 그럼으로 다음과 같이 if문을 사용해 바이트가 될수없는 수를 걸러낸다.
 		if(int_num2<Byte.MIN_VALUE||int_num2>Byte.MAX_VALUE) {
 			System.out.println("바꿀수 없는 값");
