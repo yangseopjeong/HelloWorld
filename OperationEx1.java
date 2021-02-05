2eb8f27602a20a57b07f071f5a12a4fbd31d0c1a
yangseopjeong <72899485+yangseopjeong@users.noreply.github.com>
2021-02-05 11:57:20 +0900

    Update OperationEx1.java

--- OperationEx1.java
+++ OperationEx1.java
@@ -2,8 +2,24 @@
 public class OperationEx1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num1=1;
+		int num2=3;
+		int num3=5;
+		int result=num1+num2+num3;
+		System.out.println("결과값:"+result);
+		//예제
+		int apple=83;
+		int child = 12;
+		int apple_one=apple/child;
+		int apple_remain = apple%child;
+		System.out.println("한사람당 배급된 사과수:"+apple_one+"\n남은 사과수:"+ apple_remain);
 	}
 
 }
+
+// 결과 콘솔창
+// 결과값:9
+// 한사람당 배급된 사과수:6
+// 남은 사과수:11
+
+
